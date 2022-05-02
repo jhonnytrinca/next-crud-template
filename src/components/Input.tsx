12601b3696445cd03a7231f1b42f3d779089bd06
Jhonny Trinca <jhonny.trinca@southsystem.com.br>
2022-05-02 13:25:38 -0300

    feat: readonly input bg color

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -15,7 +15,7 @@ export default function Input(props: InputProps) {
         value={props.value}
         readOnly={props.readOnly}
         className={`border border-purple-500 rounded-lg focus:outline-none bg-grey-100 px-4 py-2 ${
-          props.readOnly ? '' : 'focus:bg-white'
+          props.readOnly ? 'bg-gray-200' : 'focus:bg-white'
         }`}
         onChange={(e) => props.onChange?.(e.target.value)}
       />
